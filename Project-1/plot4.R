0af3ea637ccef0f3b04316b80c3585734bfaec24
Jawaid Siddique <jawaid@imforhelp.org>
2016-09-15 18:59:25 +0500

    Update plot4.R

--- Project-1/plot4.R
+++ Project-1/plot4.R
@@ -21,6 +21,6 @@ plot(data2$DateTime, data2$Sub_metering_1, type="l", ylab= "Energy sub metering"
 lines(data2$DateTime, data2$Sub_metering_2, type="l", col="red")
 lines(data2$DateTime, data2$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c("black", "red", "blue"))
-plot(data2$DateTime, data2$Voltage,type="l",ylab="Voltage",xlab="datetime")
-plot(data2$DateTime, data2$Global_reactive_power,type='l',xlab="datetime",ylab="Global_reactive_power")
+plot(data2$DateTime, data2$Voltage,type="l", ylab="Voltage", xlab="datetime")
+plot(data2$DateTime, data2$Global_reactive_power, type='l', xlab="datetime",ylab="Global_reactive_power")
 dev.off()
