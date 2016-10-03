28cb837f5c507b6562115091a483bbe15293f5d4
Jawaid Siddique <jawaid@imforhelp.org>
2016-10-03 14:40:20 +0500

    Commit Changes up to plot4.R

---
+++ Project-2/plot4.R
@@ -0,0 +1,22 @@
+# Across the United States, how have emissions from coal combustion-related sources changed 
+# from 1999¨C2008?
+
+setwd("C:/OPT/Data/Coursera/Data_Exp_Ana/Project-2")
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+head(SCC$Short.Name)
+
+par("mar"=c(5.1, 4.5, 4.1, 2.1))
+png(filename = "./plot4.png", width =504, height = 504, units = "px")
+coal <- grep("coal", SCC$Short.Name, ignore.case = T)
+coal <- SCC[coal, ]
+coal <- NEI[NEI$SCC %in% coal$SCC, ]
+
+coalEmissions <- aggregate(coal$Emissions, list(coal$year), FUN = "sum")
+
+plot(coalEmissions, type = "l", xlab = "Year", 
+     main = "Total Emissions From Coal Combustion-related\n Sources from 1999 to 2008", 
+     ylab = expression('Total PM'[2.5]*" Emission"))
+dev.off()
